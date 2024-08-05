6e199eb48ff2d8046fe5d0b15e5475c3c163d98d
meowmax <maximillian@disroot.org>
2024-08-05 15:52:47 +1200

    fix bug in regex whoopsie

--- bg_quiettitles.js
+++ bg_quiettitles.js
@@ -1,5 +1,5 @@
 const pattern1 = "<all_urls>"
-const pattern2 = "*://*tumblr*/*"
+const pattern2 = "*://*/*"
 
 const filter = {
     urls: [pattern1, pattern2]
@@ -9,7 +9,7 @@ function listener(tabId, changeInfo, tabInfo){
     if (tabInfo.title){
 
       if (/[(){}[\]][0-9]+.?[(){}[\]]/g.test(tabInfo.title)) {
-        console.log("status: ", /[(){}[\]][0-9]+.?[(){}[\]]/g.test(tabInfo.title))
+        console.log("status: ", /[(){}[\[][0-9]+.?[(){}[\]]/g.test(tabInfo.title))
         let executing = browser.tabs.executeScript(
           tabId,                 // optional integer
           {file: "/quiet_ur_title_pls.js"}                // object
--- quiet_ur_title_pls.js
+++ quiet_ur_title_pls.js
@@ -1,2 +1,2 @@
-document.title = document.title.replace(/[(){}[\]][0-9]+.?[(){}[\]]/g, '');
+document.title = document.title.replace(/[(){}[\[][0-9]+.?[(){}[\]]/g, '');
 console.log("[quiettitles addon] quiet your title !!")
